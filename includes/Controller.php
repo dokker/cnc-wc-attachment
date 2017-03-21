920eb4f085d6c96e0db375f01c7847cea6f4ea29
docker <dock3r@gmail.com>
2017-03-21 14:48:06 +0100

    Hook attachment to the frontend

--- includes/Controller.php
+++ includes/Controller.php
@@ -5,5 +5,16 @@ class Controller {
 	{
 		require_once('ACF.php');
 		new ACF();
+		add_action('woocommerce_product_meta_end', [$this, 'addAttachmentField']);
+	}
+
+	public function addAttachmentField()
+	{
+		?>
+		<div class="attachment">
+			<?php $wcatt = get_field('cnc-wc-attachment'); ?>
+			<a href="<?php echo $wcatt['url']; ?>" target="_blank"><?php echo $wcatt['name']; ?></a>
+		</div>
+		<?php
 	}
 }
