5cf11e3cc1769f1b95bf1c428a3482af6d89fedd
docker <dock3r@gmail.com>
2017-03-21 15:17:06 +0100

    Add style to attachment frontend

--- includes/Controller.php
+++ includes/Controller.php
@@ -6,6 +6,9 @@ class Controller {
 		require_once('ACF.php');
 		new ACF();
 		add_action('woocommerce_product_meta_end', [$this, 'addAttachmentField']);
+
+		$this->plugin_url = plugin_dir_url(dirname(__FILE__));
+		add_action('wp_enqueue_scripts', [$this, 'registerStyles']);
 	}
 
 	public function addAttachmentField()
@@ -13,8 +16,15 @@ class Controller {
 		?>
 		<div class="attachment">
 			<?php $wcatt = get_field('cnc-wc-attachment'); ?>
-			<a href="<?php echo $wcatt['url']; ?>" target="_blank"><?php echo $wcatt['name']; ?></a>
+			<a href="<?php echo $wcatt['url']; ?>" target="_blank"><?php echo (!empty($wcatt['caption'])) ? $wcatt['caption'] : $wcatt['name']; ?></a>
 		</div>
 		<?php
+	}
+
+	/**
+	 * Register styles for events related pages
+	 */
+	public function registerStyles() {
+		wp_enqueue_style( 'cnc-wcatt-style' , $this->plugin_url . 'assets/style.css');
 	}
 }
